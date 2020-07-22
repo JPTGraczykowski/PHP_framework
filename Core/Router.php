ca9761eb763bb592ac0ede48f19816b2ffa68a5c
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2020-07-22 23:00:27 +0200

    Create Front Controller and Router

---
+++ Core/Router.php
@@ -0,0 +1,76 @@
+<?php
+
+/* * * Router * * */
+
+class Router {
+
+    // Associative array of routes (the routing table): @var array
+
+    protected $routes = [];
+
+    // Parameters from the matched routes
+
+    protected $params = [];
+
+    /* 
+        Add a route to the routing table
+
+        @param string $route  The route URL
+        @param array $params Parameters(ontroller, action, etc.)
+
+        return @void
+    */
+
+    public function add($route, $params) {
+        
+        $this -> routes[$route] = $params;
+
+    }
+
+    /*  
+        Get all the routes from the routing table
+
+        return array
+    */
+
+    public function getRoutes() {
+
+        return $this -> $routes;
+
+    }
+
+    /*  
+        Match the route to the routes in the routing table, setting the $params
+        property if a rute is found
+
+        @param string $url the URL route
+        @return boolean if a match was found
+
+        return array
+    */
+
+    public function match($url) {
+
+        foreach($this -> routes as $route => $params) {
+            if ($url == $route) {
+                $this -> params = $params;
+                return true;
+            }
+        }
+        return false;
+        
+    }
+
+    /*
+        Get the currently matched parameters
+
+        @return array
+    */
+
+    public function getParams() {
+
+        return $this->params;
+
+    }
+
+}
